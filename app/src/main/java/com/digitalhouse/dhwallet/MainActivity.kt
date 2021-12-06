ad924aeecae1dd5f7aa37895e566aa3072faec45
Victor Carreira <sg.el.carreira@hotmail.com>
2021-12-05 23:16:57 -0300

    criada a seção tela de transferencia,precisa melhora cantos arrendodados do layout item_contato

--- app/src/main/java/com/digitalhouse/dhwallet/MainActivity.kt
+++ app/src/main/java/com/digitalhouse/dhwallet/MainActivity.kt
@@ -2,10 +2,17 @@ package com.digitalhouse.dhwallet
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.FragmentContainerView
 
 class MainActivity : AppCompatActivity() {
+
+    //private val containerFragmentoTela: FragmentContainerView by lazy { findViewById(R.id.container_fragemento_tela) }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.container_fragemento_tela,TelaTransferirFragment())
+            .commit()
     }
 }
